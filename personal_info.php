4958d5d503bfcdecc6f61b75cfba09e8c885dde5
iftun <72260688+iftuun@users.noreply.github.com>
2023-09-14 21:32:55 +0600

    Add files via upload

---
+++ personal_info.php
@@ -0,0 +1,7 @@
+<?php
+// Define variables with your personal information
+$name = "Ifthakhar Ahmed";
+$age = 27;
+$country = "Bangladesh";
+$introduction = "Hello! I'm Ifthakhar, and I'm from Bangladesh. I'm currently 27 years old.";
+?>
